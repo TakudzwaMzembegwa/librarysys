380c26bd337440e7f8b1bab0ca846210b9dec339
TeeKay <takudzwamzembegwa@gmail.com>
2021-08-22 17:26:27 +0200

    initial commit

---
+++ backend/src/main/java/com/librarysys/backend/LibrarySysApplication.java
@@ -0,0 +1,13 @@
+package com.librarysys.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibrarySysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibrarySysApplication.class, args);
+	}
+
+}
