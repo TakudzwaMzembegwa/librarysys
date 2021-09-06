38f5b7ba2ddf9ffab0a44e62e8458e5d16cd756a
Sango <60422984+MatyilaSango@users.noreply.github.com>
2021-09-06 17:41:13 +0200

    Added function to return and push a category.

--- BugBusterLibrary/src/bugbusterlibrary/dao/CategoryDao.java
+++ BugBusterLibrary/src/bugbusterlibrary/dao/CategoryDao.java
@@ -4,11 +4,41 @@
  * and open the template in the editor.
  */
 package bugbusterlibrary.dao;
+import bugbusterlibrary.entity.Category;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
 /**
  *
  * @author Takudzwa Mzembegwa
+ * @author Sango
  */
 public class CategoryDao {
+    
+     //Return list of Category
+    public List<Category> findAllCategories(){
+        EntityManagerFactory emf=Persistence.createEntityManagerFactory("BugBusterLibraryPU");  
+        EntityManager em=emf.createEntityManager();
+        List<Category> category = em.createQuery("SELECT c FROM Category c", Category.class).getResultList();
+        return category;
+    } 
+    
+    //Add a category in the category list
+    public void PushInCategory(Integer categoryId, String faculty, String department, String module) {
+        Category category = new Category();
+        category.setCategoryId(categoryId);
+        category.setFaculty(faculty);
+        category.setDepartment(department);
+        category.setModule(module);
+        
+        EntityManagerFactory emf=Persistence.createEntityManagerFactory("BugBusterLibraryPU");  
+        EntityManager em=emf.createEntityManager();
+        em.getTransaction().begin();
+        em.persist(category);
+        em.getTransaction().commit();
+        em.clear();
+    }
     
 }
