3b596c9df6d14d7d963021a76aeff9892a82ed6b
Sango <60422984+MatyilaSango@users.noreply.github.com>
2021-09-06 17:54:39 +0200

    Added function to return all users.

--- BugBusterLibrary/src/bugbusterlibrary/dao/UserDao.java
+++ BugBusterLibrary/src/bugbusterlibrary/dao/UserDao.java
@@ -4,11 +4,24 @@
  * and open the template in the editor.
  */
 package bugbusterlibrary.dao;
+import bugbusterlibrary.entity.User;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
 /**
  *
  * @author Takudzwa Mzembegwa
  */
 public class UserDao {
+    
+    //Return list of all Users
+    public List<User> findAllUsers(){
+        EntityManagerFactory emf=Persistence.createEntityManagerFactory("BugBusterLibraryPU");  
+        EntityManager em=emf.createEntityManager();
+        List<User> user = em.createQuery("SELECT u FROM User u", User.class).getResultList();
+        return user;
+    }
     
 }
