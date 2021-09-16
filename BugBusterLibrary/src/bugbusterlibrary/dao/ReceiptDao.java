388d4edd2e7351e27a9bb874454634908367751c
TeeKay <takudzwamzembegwa@gmail.com>
2021-09-16 16:28:05 +0200

    Added java doumantantion and refactored

--- BugBusterLibrary/src/bugbusterlibrary/dao/ReceiptDao.java
+++ BugBusterLibrary/src/bugbusterlibrary/dao/ReceiptDao.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package bugbusterlibrary.dao;
+
 import bugbusterlibrary.EntityManagerFactoryHandler;
 import bugbusterlibrary.entity.Book;
 import bugbusterlibrary.entity.Receipt;
@@ -20,66 +21,76 @@ import javax.persistence.Persistence;
  * @author Sango
  */
 public class ReceiptDao {
-    
+
     /**
-      * @return list of all Receipts
-      */
-    public List<Receipt> findAll(){  
-        EntityManager em =EntityManagerFactoryHandler.getEntityManagerFactory().createEntityManager();
+     * @return list of all Receipts
+     */
+    public List<Receipt> findAll() {
+        EntityManager em = EntityManagerFactoryHandler.getEntityManagerFactory().createEntityManager();
         List<Receipt> receipt = em.createQuery("SELECT r FROM Receipt r", Receipt.class).getResultList();
         return receipt;
-    }  
-    
-     /**
-      * @param user the user whose receipts are to be found
-      * @return user's receipts
-      */
-    public List<Receipt> findUserReceipts(User user){  
+    }
+
+    /**
+     * @param user the user whose receipts are to be found
+     * @return user's receipts
+     */
+    public List<Receipt> findUserReceipts(User user) {
         EntityManager em = EntityManagerFactoryHandler.getEntityManagerFactory().createEntityManager();
-        List<Receipt> userRecepits = em.createQuery("SELECT r FROM Receipt r WHERE r.receiptId = :receiptId", Receipt.class).setParameter("receiptId", user.getUserId()).getResultList();
+        List<Receipt> userRecepits = em
+                .createQuery("SELECT r FROM Receipt r WHERE r.receiptId = :receiptId", Receipt.class)
+                .setParameter("receiptId", user.getUserId()).getResultList();
         return userRecepits;
     }
-    
+
     /**
-      * Adds a receipt to the database
-      */
-    public void persist(Long receiptId, Date dateLoaned, Date dateReturned, float fineDue, Book bookId, User userId){
+     * Adds a receipt to the database
+     */
+    public void persist(Long receiptId, Date dateLoaned, Date dateReturned, float fineDue, Book bookId, User userId) {
         Receipt receipt = new Receipt(receiptId, dateLoaned, dateReturned, fineDue);
         receipt.setBookId(bookId);
         receipt.setUserId(userId);
-          
+
         EntityManager em = EntityManagerFactoryHandler.getEntityManagerFactory().createEntityManager();
         em.getTransaction().begin();
         em.persist(receipt);
         em.getTransaction().commit();
         em.clear();
     }
-    
-    // function to delete a receipt from the database. 
-    public void deleteReceipt(Long receiptID) 
-    {
-       EntityManager em = EntityManagerFactoryHandler.getEntityManagerFactory().createEntityManager();
-       Receipt receipt = em.find(Receipt.class, receiptId); // search the database for a receipt with a specific ID. 
-        
-       if (receipt != null) // see if the receipt ID exists.  
-       {
+
+    /**
+     * Deletes a receipt from the database.
+     * 
+     * @param receiptID the id of the receipt to be deleted
+     */
+    public void deleteReceipt(Long receiptID) {
+        EntityManager em = EntityManagerFactoryHandler.getEntityManagerFactory().createEntityManager();
+        Receipt receipt = em.find(Receipt.class, receiptID); // search the database for a receipt with a specific ID.
+
+        if (receipt != null) // see if the receipt ID exists.
+        {
             em.getTransaction().begin();
-            em.remove(receipt); // delete the receipt. 
+            em.remove(receipt); // delete the receipt.
             em.getTransaction().commit();
         }
     }
 
-    // funtion to update a receipt. 
-    public void updateReceipt(Receipt myReceipt, Book bookID, User userID)
-    {
-        // set the new values of the receipt. 
-    	Receipt receipt = myReceipt;
-    	receipt.setBookId(bookID);
-    	receipt.setUserId(userID);
-        
+    /**
+     * Updates a receipt.
+     * 
+     * @param myReceipt the Receipt object to be updated
+     * @param bookID    the book to be added to the receipt
+     * @param userID    the book to be added to the receipt
+     */
+    public void updateReceipt(Receipt myReceipt, Book bookID, User userID) {
+        // set the new values of the receipt.
+        Receipt receipt = myReceipt;
+        receipt.setBookId(bookID);
+        receipt.setUserId(userID);
+
         EntityManager em = EntityManagerFactoryHandler.getEntityManagerFactory().createEntityManager();
         em.getTransaction().begin();
-        em.merge(receipt); // update the receipt. 
+        em.merge(receipt); // update the receipt.
         em.getTransaction().commit();
     }
 }
--- BugBusterLibrary/src/bugbusterlibrary/dao/UserDao.java
+++ BugBusterLibrary/src/bugbusterlibrary/dao/UserDao.java
@@ -4,7 +4,9 @@
  * and open the template in the editor.
  */
 package bugbusterlibrary.dao;
+
 import bugbusterlibrary.EntityManagerFactoryHandler;
+import bugbusterlibrary.entity.Receipt;
 import bugbusterlibrary.entity.User;
 import java.util.List;
 import javax.persistence.EntityManager;
@@ -17,51 +19,59 @@ import javax.persistence.Persistence;
  * @author Sango
  */
 public class UserDao {
-    
+
     /**
      * @return list of all Users
      */
-    public List<User> findAll(){
+    public List<User> findAll() {
         EntityManager em = EntityManagerFactoryHandler.getEntityManagerFactory().createEntityManager();
         List<User> user = em.createQuery("SELECT u FROM User u", User.class).getResultList();
         return user;
     }
 
     /**
-     * @param username the username of the user to be found  
-     * @return User 
+     * @param username the username of the user to be found
+     * @return User
      */
-    public User findByUsername(String username){
+    public User findByUsername(String username) {
         EntityManager em = EntityManagerFactoryHandler.getEntityManagerFactory().createEntityManager();
-        User user = em.createNamedQuery("User.findByUsername", User.class).setParameter("username", username).getSingleResult();
+        User user = em.createNamedQuery("User.findByUsername", User.class).setParameter("username", username)
+                .getSingleResult();
         return user;
     }
-    
-    // funtion to delete a specific user. 
-    public void deleteUser(Long userID) 
-    {
-       EntityManager em = EntityManagerFactoryHandler.getEntityManagerFactory().createEntityManager();
-       User user = em.find(User.class, userID); // search for the user by ID. 
-        
-       if (user != null) // see if they are in the database. 
-       {
+
+    /**
+     * Deletes a user specific from the database.
+     * 
+     * @param userID the id of the user to be deleted
+     */
+    public void deleteUser(Long userID) {
+        EntityManager em = EntityManagerFactoryHandler.getEntityManagerFactory().createEntityManager();
+        User user = em.find(User.class, userID); // search for the user by ID.
+
+        if (user != null) // see if they are in the database.
+        {
             em.getTransaction().begin();
-            em.remove(user); // then remove them. 
+            em.remove(user); // then remove them.
             em.getTransaction().commit();
         }
     }
 
-    // function to update a user's details. 
-    public void updateUser(User myUser, List<Receipt> receiptList)
-    {
-        // set the new information. 
-    	User user = myUser;
-    	receipt.setReceiptList(receiptList);
-        
+    /**
+     * Updates user's details.
+     * 
+     * @param myUser      the User object to be updated
+     * @param receiptList list of users to be added to the user
+     */
+    public void updateUser(User myUser, List<Receipt> receiptList) {
+        // set the new information.
+        User user = myUser;
+        user.setReceiptList(receiptList);
+
         EntityManager em = EntityManagerFactoryHandler.getEntityManagerFactory().createEntityManager();
         em.getTransaction().begin();
-        em.merge(user); // update it. 
+        em.merge(user); // update it.
         em.getTransaction().commit();
     }
-    
+
 }
